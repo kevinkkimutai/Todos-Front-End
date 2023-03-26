ffe0d81362e8da9db1491e43b2ef99f48f8c1011
kevinkkimutai <kiruikevinkim@gmail.com>
2023-03-26 22:49:16 +0300

    routes added

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
-import SignUp from "./SignUp";
 import Login from "./Login";
 import NavBar from "./NavBar";
 import Home from "./Home";
